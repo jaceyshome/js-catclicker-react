8ffdd2bf00d38c5620671d205e2874dad2ab3b88
Albin <albin.nilsson89@gmail.com>
2015-05-15 14:42:52 +0200

    added new controller and model

---
+++ api/controllers/BearsController.js
@@ -0,0 +1,60 @@
+/**
+ * BearsController
+ *
+ * @description :: Server-side logic for managing bears
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+
+    /**
+     * `BearsController.index()`
+     */
+    index: function (req, res) {
+        Bear.find()
+            .exec(function (err, bears) {
+
+                return res.view('bears/index', {
+                    bears: bears,
+                })
+
+            });
+    },
+
+
+    /**
+     * `BearsController.show()`
+     */
+    show: function (req, res) {
+        Bear.create({
+            name: 'Albin'
+        })
+            .exec(function (err, bear) {
+                return res.json({
+                    ok: 'ok'
+                })
+            });
+    },
+
+
+    /**
+     * `BearsController.edit()`
+     */
+    edit: function (req, res) {
+        return res.json({
+            todo: 'edit() is not implemented yet!'
+        });
+    },
+
+
+    /**
+     * `BearsController.delete()`
+     */
+    delete: function (req, res) {
+        return res.json({
+            todo: 'delete() is not implemented yet!'
+        });
+    }
+};
+
